44d6ede58dddc73fecc2f9c0fbf41925a0c1e8d8
vulild <vulild@hotmail.com>
2020-07-05 22:25:37 +0800

    修改ui项目类型为mvc

--- Quartz.UI/Startup.cs
+++ Quartz.UI/Startup.cs
@@ -5,15 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Quartz.Tasks;
-using Vulild.Service;
-using Vulild.Service.Quartz;
-using Vulild.Service.TaskService;
 
 namespace Quartz.UI
 {
@@ -29,7 +23,7 @@ namespace Quartz.UI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,24 +33,14 @@ namespace Quartz.UI
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            ServiceUtil.SearchAssmbly();
-            ServiceUtil.InitService("taskservice", new QuartOption()
+            else
             {
-                ConnectionString = "Server=vulild.top;Database=Quartz;Uid=vulild;Pwd=gelz1122"
-            });
-
-            var service = ServiceUtil.GetService<ITaskService>();
-            service.UpdateTask(new QuartzTask
-            {
-                GroupName = "TaskGroup",
-                JobName = "TaskJob",
-                TaskType = typeof(DemoTask),
-                Cron = "0/5 * * * * ?"
-            });
-            service.StartTask();
-
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
             app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -64,7 +48,9 @@ namespace Quartz.UI
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
